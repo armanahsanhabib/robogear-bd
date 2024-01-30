2c1921193e8f67f98454b8ab07d2bb0434ea9481
Ahsan Habib <arman0sheikh@gmail.com>
2024-01-31 02:47:41 +0600

    footer font changed to small

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -7,18 +7,20 @@ const Footer = () => {
     <footer>
       <div className="footer-top">
         <div className="container px-5 mx-auto">
-          <div className="top grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-[100px] gap-x-[50px] gap-y-[50px] py-10 text-center lg:text-start">
+          <div className="top md:text-base text-sm grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 xl:gap-x-[100px] gap-x-[50px] md:gap-y-[50px] gap-y-8 py-10 text-center lg:text-start">
             <div className="logo">
               <img
                 src={Logo}
                 alt=""
-                className="xl:h-[64px] h-[50px] mb-2 mx-auto lg:mx-0"
+                className="xl:h-[64px] md:h-[50px] h-[35px] mb-2 mx-auto lg:mx-0"
               />
               <p className="text-gray-500 mb-5 font-[300]">
                 RoboGear BD is Bangladesh&apos;s most popular engineering and
                 robotics online shop.
               </p>
-              <h2 className="text-lg font-semibold mb-2">Found Us</h2>
+              <h2 className="md:text-lg text-base font-semibold mb-2">
+                Found Us
+              </h2>
               <ul className="flex items-center gap-x-3 justify-center lg:justify-start">
                 <li>
                   <a href="/" className="xl:text-3xl text-2xl text-sky-700">
@@ -38,8 +40,8 @@ const Footer = () => {
               </ul>
             </div>
             <div className="col">
-              <h3 className="font-semibold mb-6">Categories</h3>
-              <ul className="flex flex-col gap-y-3 font-[300] text-gray-500">
+              <h3 className="font-semibold md:mb-6 mb-3">Categories</h3>
+              <ul className="flex flex-col md:gap-y-3 gap-y-1 font-[300] text-gray-500">
                 <li>
                   <a href="/">Arduino and boards</a>
                 </li>
@@ -55,8 +57,8 @@ const Footer = () => {
               </ul>
             </div>
             <div className="col">
-              <h3 className="font-semibold mb-6">Useful Links</h3>
-              <ul className="flex flex-col gap-y-3 font-[300] text-gray-500">
+              <h3 className="font-semibold md:mb-6 mb-3">Useful Links</h3>
+              <ul className="flex flex-col md:gap-y-3 gap-y-1 font-[300] text-gray-500">
                 <li>
                   <a href="/">Terms and Conditions</a>
                 </li>
@@ -72,8 +74,8 @@ const Footer = () => {
               </ul>
             </div>
             <div className="col">
-              <h3 className="font-semibold mb-6">Reach Us</h3>
-              <ul className="flex flex-col gap-y-3 font-[300] text-gray-500">
+              <h3 className="font-semibold md:mb-6 mb-3">Reach Us</h3>
+              <ul className="flex flex-col md:gap-y-3 gap-y-1 font-[300] text-gray-500">
                 <li>
                   <a href="/">Like our Facebook Page</a>
                 </li>
@@ -91,9 +93,9 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="footer-bototm border-t py-5">
+      <div className="footer-bototm border-t py-5 font-[300]">
         <div className="container mx-auto px-5 flex flex-col lg:flex-row items-center justify-between gap-3">
-          <div className="left text-gray-500 text-sm sm:text-base text-center">
+          <div className="left text-gray-500 sm:text-base text-sm text-center">
             {`\u00A9 2023 - ${new Date().getFullYear()}`} RoboGearBD - All
             Rights Reserved.
           </div>
